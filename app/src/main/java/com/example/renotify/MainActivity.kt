5d5f08a51479da4c91063f2dbf5dbcab9363706f
niteryt <vaguex1@proton.me>
2024-09-25 00:47:01 +0530

    Check for notification permission

--- app/src/main/java/com/example/renotify/MainActivity.kt
+++ app/src/main/java/com/example/renotify/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.example.renotify
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationManagerCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -18,7 +23,13 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        val intent = Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS")
-        startActivity(intent)
+        val packageNames = NotificationManagerCompat.getEnabledListenerPackages(this)
+        if (!packageNames.contains(packageName)) {
+            val intent = Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS")
+            startActivity(intent)
+        } else {
+            val restartIntent = Intent(this, NotificationListener::class.java)
+            startService(restartIntent)
+        }
     }
 }
